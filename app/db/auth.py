eb59cefcee2ddefad5d3ee9dffec341932054ea5
ibrahim-stepsharp <m.ibrahim@stepsharp.dev>
2025-09-08 17:51:00 +0500

    first commit

---
+++ app/db/auth.py
@@ -0,0 +1,11 @@
+def get_user_id_from_token(token: str) -> str | None:
+    """
+    This is a placeholder for the actual token validation logic.
+    - Validate the token.
+    - Return the user ID if the token is valid.
+    - Return None if the token is invalid.
+    """
+    # Example:
+    if token == "valid_token_xyz789":
+        return "user_123"
+    return None
---
+++ app/schemas/config_schema.py
@@ -0,0 +1,35 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Application details
+    APP_TITLE: str = "FastApi_Foundation"
+    APP_VERSION: str = "Beta_1.0.0"
+    APP_DESCRIPTION: str = "APIs for the FastApi_Foundation"
+
+    # CORS
+    CORS_ALLOWED_ORIGINS: str = "*"
+    CORS_ALLOW_CREDENTIALS: bool = True
+    CORS_ALLOWED_METHODS: str = "*"
+    CORS_ALLOWED_HEADERS: str = "*"
+
+    # Docs Auth
+    DOCS_USERNAME: str = "admin"
+    DOCS_PASSWORD: str = "admin"
+
+    # Database
+    DB_HOST: str
+    DB_PORT: int = 5432
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DATABASE_INIT: bool = False
+
+    # Logging
+    LOGLEVEL: str = "INFO"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+        extra = 'ignore'
+
+settings = Settings()
